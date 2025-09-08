c0a898f656bcee35b6ce26c138743f60ce9a2673
tntspider76 <tntspider0206@gmail.com>
2025-09-08 12:50:03 +0800

    add new data

--- folderCreatTest.m
+++ folderCreatTest.m
@@ -1,25 +1,68 @@
-close all 
 clc
 clearvars
+close all
 
-MaxAlt = 100;
-MinAlt = 50;
-dataDistence = 5;
-
-firstLayer = ["test1","test2"];
-secondLayer = ["surf","2D"];
-thirdLayer  = linspace(MinAlt,MaxAlt,((MaxAlt - MinAlt)/dataDistence)+1);
-
-for i = 1:length(firstLayer)
-    if ~exist(firstLayer(i),'dir'), mkdir(firstLayer(i)); end
-    for j = 1:length(secondLayer)
-        Ndir = append(firstLayer(i),"/",secondLayer(j));
-        if ~exist(Ndir,'dir'), mkdir(Ndir); end
-        for k = 1:length(thirdLayer)
-            Ndir = append(firstLayer(i),"/",secondLayer(j),"/",string(thirdLayer(k)));
-            if ~exist(Ndir,'dir'), mkdir(Ndir); end
+Bu = [];
+Bv = []; 
+Bw = [];
+files = {'Vector/0.txt'};
+% = {'Vector/0.txt','Vector/10.txt'};
+%heights = [0, 10]; 
+heights = [0]; 
+longitude = []; % 經度
+latitude = []; % 緯度
+Z = [];
+for f = 1:length(files)
+    data = readmatrix(files{f});
+    for i=1:height(data)
+        if mod(data(i,1),10) == 0.5 && mod(data(i,2),10) == 0
+            longitude = [longitude;data(i,1)]; % 經度
+            latitude = [latitude;data(i,2)]; % 緯度
+            Bu = [Bu;data(i,3)]; % 總磁場強度
+            Bv = [Bv;data(i,4)]; % 總磁場強度
+            Bw = [Bw;data(i,5)]; % 總磁場強度
+            Z  = [Z;  heights(f)];
         end
     end
 end
+mag = sqrt(Bu.^2 + Bv.^2 + Bw.^2);
+Bu = Bu ./ mag;
+Bv = Bv ./ mag;
+Bw = Bw ./ mag;
 
-%if ~exist('fig','dir'), mkdir('fig'); end
+
+magZ = Bw; % 使用 Z 分量
+magZnorm = magZ / max(abs(magZ));
+
+cmap = jet(256);
+colorIdx = round((magZnorm + 1) / 2 * 255) + 1;
+
+f1 = figure(Theme="light"); hold on;
+for k = 1:length(Bu)
+    quiver(longitude(k)/10, latitude(k)/10, Bu(k), Bv(k), 0, ...
+        'Color', cmap(colorIdx(k),:), 'LineWidth', 0.5, 'Marker',".");
+end
+colormap(cmap);
+colorbar;
+clim([-1 1]);
+xlim([0 36]);
+ylim([-9 9]);
+xlabel('Longitude/10');
+ylabel('Latitude/10');
+title('2D Magnetic Field (Color Represents Z Component)');
+
+
+
+A=1;
+if A==0
+    f2 = figure(Theme="light");
+    quiver3(longitude, latitude, Z, Bu, Bv, Bw,0); 
+    axis([min(longitude) max(longitude) ...
+        min(latitude)  max(latitude)  ...
+        -10 10]);       % 視情況調整Z範圍
+    xlabel('Longitude');
+    ylabel('Latitude');
+    %zlabel('Altitude (km)');
+    title('Magnetic Field Directions Only');
+end
+
